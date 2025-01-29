3e1389a5c352e4fe02226698fea327d323f6e0a1
nathlessa <nathlessasa@gmail.com>
2025-01-29 20:36:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const searchButton = document.querySelector("#search-input")
+
+searchButton.addEventListener("click", function (event) {
+
+    //event.preventDefault();
+    
+    alert("Botão em implementação.");
+    
+})
